e0e29058bf979cb96992753c050cc37bd48f6a69
Andrea <deangelisandrea16@icloud.com>
2025-02-27 03:18:25 +0100

    Fix porta per Render

--- app.py
+++ app.py
@@ -52,4 +52,8 @@ def send_message(to, text):
     print("Risposta inviata:", response.json())
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    import os
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port, debug=True)
+
